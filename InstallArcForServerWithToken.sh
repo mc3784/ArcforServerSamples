c59b8937b702a65f1065dde4ce86a37baf2a404f
mc3784 <mc3784@nyu.edu>
2025-02-19 22:29:05 +0000

    just export the ARC variable

--- InstallArcForServerWithToken.sh
+++ InstallArcForServerWithToken.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
-cloudEnv="AzureCloud"
-
-# Get the username of the user who invoked sudo
-user=$(logname)
-
-# Add the environment variable to the user's .bashrc file
-sudo -u $user bash -c 'echo "export MSFT_ARC_TEST=\"true\"" >> $HOME/.bashrc'
-
-# Source the .bashrc file to apply the changes
-sudo -u $user bash -c 'source $HOME/.bashrc'
+export MSFT_ARC_TEST=true
 
 while getopts "c:p:" opt; do
   case $opt in
