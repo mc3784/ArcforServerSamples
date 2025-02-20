4ec3d0df091a44a3d39f67830c286a0ce4b38e5a
mc3784 <mc3784@nyu.edu>
2025-02-20 14:57:34 +0000

    add principalId

--- InstallArcForServerMultiTenantDebug2.sh
+++ InstallArcForServerMultiTenantDebug2.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export MSFT_ARC_TEST=true
+
+if [ -z "$MSFT_ARC_TEST" ]; then
+  echo "Error: MSFT_ARC_TEST is not set."
+  exit 1
+fi
+
 principalId="$2"
 
 if [ -z "$principalId" ]; then
