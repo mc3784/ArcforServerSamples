7c372ae3289205ba5837f5d97974043901320892
mc3784 <mc3784@nyu.edu>
2025-03-03 15:37:01 +0000

    debug

--- InstallArcForServerMultiTenantDebug.sh
+++ InstallArcForServerMultiTenantDebug.sh
@@ -6,16 +6,27 @@ if [ -z "$principalId" ]; then
 fi
 
 get_machine_details() {
-  local principalId=$1
+  principalId=$1
 
-  # get token
-  content=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&object_id=$principalId&resource=$AZUREURI")
-  access_token=$(echo $content | jq -r '.access_token')
+  # Get token
+  token_response=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&object_id=$principalId&resource=$AZUREURI")
+  access_token=$(echo "$token_response" | jq -r '.access_token')
 
-  # machine details
-  content=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
-  resourceId=$(echo $content | jq -r '.compute.resourceId')
-  resourceLocation=$(echo $content | jq -r '.compute.location')
+  if [ -z "$access_token" ]; then
+    echo "Failed to retrieve access token" >&2
+    return 1
+  fi
+
+  # Get machine details
+  metadata_response=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
+  resourceId=$(echo "$metadata_response" | jq -r '.compute.resourceId')
+  resourceLocation=$(echo "$metadata_response" | jq -r '.compute.location')
+
+  if [ -z "$resourceId" ] || [ -z "$resourceLocation" ]; then
+    echo "Failed to retrieve machine details" >&2
+    return 1
+  fi
+
   IFS='/' read -r -a resourceIdArray <<< "$resourceId"
   subscriptionID=${resourceIdArray[2]}
   resourceGroupName=${resourceIdArray[4]}
@@ -23,24 +34,4 @@ get_machine_details() {
   echo "$access_token,$subscriptionID,$resourceGroupName,$resourceLocation"
 }
 
-
-retryCount=5
-sleepSeconds=3
-
-export TENANT_ID="72f988bf-86f1-41af-91ab-2d7cd011db47"
-export AUTH_TYPE="principal"
-export CORRELATION_ID="c0a82881-305f-4243-b9e3-96861a595b7e"
-export CLOUD="AzureCloud"
-export AZUREURI="https://management.azure.com/"
-export HCRPURI="https://aka.ms/azcmagent-windows"
-
-while [ $retryCount -gt 0 ]; do
-  machine_info=$(get_machine_details "$principalId")
-  if [ $? -eq 0 ]; then
-    break
-  else
-    echo "Error getting machine details. Retrying in $sleepSeconds seconds..."
-    sleep $sleepSeconds
-    retryCount=$((retryCount-1))
-  fi
-done
+machine_info=$(get_machine_details "$principalId")
