6d4819ddccb1e59fb4851c8c5a07fa9d44340673
mc3784 <mc3784@nyu.edu>
2025-03-03 21:39:32 +0000

    debug

--- InstallArcForServerMultiTenantDebug.sh
+++ InstallArcForServerMultiTenantDebug.sh
@@ -5,33 +5,55 @@ if [ -z "$principalId" ]; then
   exit 1
 fi
 
+export AZUREURI="https://management.azure.com/"
+
 get_machine_details() {
-  principalId=$1
+  local principalId=$1
 
-  # Get token
-  token_response=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&object_id=$principalId&resource=$AZUREURI")
-  access_token=$(echo "$token_response" | jq -r '.access_token')
+  # get token
+  content=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&object_id=$principalId&resource=$AZUREURI")
+  access_token=$(echo $content | jq -r '.access_token')
 
-  if [ -z "$access_token" ]; then
-    echo "Failed to retrieve access token" >&2
-    return 1
-  fi
+  # decode token to get tenantId
+ tenantId=$(echo $access_token | cut -d '.' -f2 | sed 's/\-/+/g; s/_/\//g' | awk '{ len=length($0) % 4; if (len == 2) { print $0"=="; } else if (len == 3) { print $0"="; } else { print $0; } }' | base64 --decode | jq -r '.tid')
 
-  # Get machine details
-  metadata_response=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
-  resourceId=$(echo "$metadata_response" | jq -r '.compute.resourceId')
-  resourceLocation=$(echo "$metadata_response" | jq -r '.compute.location')
-
-  if [ -z "$resourceId" ] || [ -z "$resourceLocation" ]; then
-    echo "Failed to retrieve machine details" >&2
-    return 1
-  fi
-
+  # machine details
+  content=$(curl -s -H "Metadata:true" "http://169.254.169.254/metadata/instance?api-version=2021-02-01")
+  resourceId=$(echo $content | jq -r '.compute.resourceId')
+  location=$(echo $content | jq -r '.compute.location')
+  imageOffer=$(echo $content | jq -r '.compute.storageProfile.imageReference.offer')
   IFS='/' read -r -a resourceIdArray <<< "$resourceId"
-  subscriptionID=${resourceIdArray[2]}
-  resourceGroupName=${resourceIdArray[4]}
-
-  echo "$access_token,$subscriptionID,$resourceGroupName,$resourceLocation"
+  subscriptionId=${resourceIdArray[2]}
+  resourceGroup=${resourceIdArray[4]}
+  
+  echo "$access_token,$tenantId,$subscriptionId,$resourceGroup,$location,$imageOffer"
 }
 
-machine_info=$(get_machine_details "$principalId")
+retryCount=5
+sleepSeconds=3
+while [ $retryCount -gt 0 ]; do
+  machine_info=$(get_machine_details "$principalId")
+  if [ $? -eq 0 ]; then
+    break
+  else
+    echo "Error getting machine details. Retrying in $sleepSeconds seconds..."
+    sleep $sleepSeconds
+    retryCount=$((retryCount-1))
+  fi
+done
+
+
+IFS=',' read -r access_token tenantId subscriptionId resourceGroup location imageOffer <<< "$machine_info"
+
+
+export MSFT_ARC_TEST=true
+sudo systemctl stop walinuxagent
+sudo systemctl disable walinuxagent
+
+sudo ufw --force enable
+sudo ufw deny out from any to 169.254.169.254
+sudo ufw default allow incoming
+
+export authType="principal";
+export correlationId="b4975a09-15b5-4e8a-be6c-322c4eef7dad";
+export cloud="AzureCloud";
